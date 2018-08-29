8fcd1fe2619c595e74630ec34e929d1a0e169217
Lloyd Saldanha <lloydsaldanha@gmail.com>
2018-08-29 19:15:38 +0000

    disabled video frame

--- capture.py
+++ capture.py
@@ -35,7 +35,7 @@ while( int(time.time() - startTime) <= captureDuration ):
         out.write(frame)
 
         # show a frame on the screen
-        cv2.imshow('frame', frame)
+#        cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
